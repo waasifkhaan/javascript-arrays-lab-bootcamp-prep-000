c99584ef952d7d984c13faaf85d39ae0c6557882
Wasif Khan <waasif.khaan@gmail.com>
2018-04-13 17:24:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(name){
-  kittens.push('Ralph')
+  kittens.push(name)
   return kittens
 
 }
 function destructivelyPrependKitten(name){
-  kittens.unshift('Bob')
+  kittens.unshift(name)
   return kittens
 }
 function destructivelyRemoveLastKitten() {
